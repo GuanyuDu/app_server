15d1ad18d7f18563a579d8c59e55110de0b7afb0
GuanyuPC <im.dugy@foxmail.com>
2021-09-13 22:14:42 +0800

    temp commit

--- src/main/java/com/guanyu/app/controller/FeedbackController.java
+++ src/main/java/com/guanyu/app/controller/FeedbackController.java
@@ -1,18 +1,21 @@
 package com.guanyu.app.controller;
 
 import com.guanyu.app.model.base.Result;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 用户信息反馈接口
+ *
  * @author Guanyu
  */
 @RestController
-@RequestMapping("api/feedback")
+@RequestMapping("/feedback")
 public class FeedbackController {
 
-    @RequestMapping("/add")
+    @PostMapping("")
     public Result<Object> addFeedback(@RequestParam String content) {
 
         return null;
