6dd0eea9e982985e7ef0e724ef2826c49446e2b6
Guanyu <im.dugy@foxmail.com>
2021-12-03 01:59:19 +0800

    添加抖音分享链接转换逻辑

--- src/main/java/com/guanyu/app/service/SubFunctionService.java
+++ src/main/java/com/guanyu/app/service/SubFunctionService.java
@@ -1,14 +1,23 @@
 package com.guanyu.app.service;
 
-import com.guanyu.app.model.base.Result;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.guanyu.app.constant.ErrorCode;
+import com.guanyu.app.model.dto.base.Result;
+import com.guanyu.app.util.net.HttpRequest;
+import com.guanyu.app.util.net.HttpResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 小程序杂七杂八功能的服务层
  *
  * @author Guanyu
  */
+@Service
 public class SubFunctionService {
 
     /**
@@ -18,6 +27,31 @@ public class SubFunctionService {
      */
     public Result<List<String>> tikTokShareLinkTransform(String shareLink) {
 
-        return Result.ok();
+        final String url = "http://iqiuz.xyz:9999/nwm";
+
+        Map<String, String> params = new HashMap<>(16);
+        params.put("text", shareLink);
+
+        String result = HttpRequest.post(url, params, Collections.emptyMap()).dataOrNull();
+
+        if (StringUtils.isNotEmpty(result)) {
+            JSONObject obj = JSON.parseObject(result);
+
+            if (obj != null && !obj.isEmpty()) {
+                Integer code = obj.getInteger("code");
+                if (code != null && code != 0) {
+                    if (code == -1) {
+                        return Result.fail(ErrorCode.SHARE_LINK_FORMAT_ERROR);
+                    }
+                    return Result.fail(code, obj.getString("msg"));
+                }
+                JSONArray data = obj.getJSONArray("data");
+
+                if (data != null && !data.isEmpty()) {
+                    return Result.ok(data.toJavaList(String.class));
+                }
+            }
+        }
+        return Result.fail(ErrorCode.DEPENDENT_SERVICE_ERROR);
     }
 }
