f4b169907aa002ac8092d947422edca2e7eec283
Guanyu <im.dugy@foxmail.com>
2023-07-27 10:21:16 +0800

    修改 Caffeine Cache 配置

--- src/main/java/com/guanyu/app/config/CaffeineConfiguration.java
+++ src/main/java/com/guanyu/app/config/CaffeineConfiguration.java
@@ -2,14 +2,22 @@ package com.guanyu.app.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.Cache;
+import com.guanyu.app.util.UserContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class CaffeineConfiguration {
 
     @Bean
-    public Cache<String, String> userCache() {
-        return Caffeine.newBuilder().maximumSize(1000).build();
+    public Cache<String, UserContext.UserInfo> userCache() {
+        // 用户缓存在 24h 后清空，超过 1000 条后会清理老数据
+        return Caffeine.newBuilder()
+                .initialCapacity(128)
+                .expireAfterWrite(24, TimeUnit.HOURS)
+                .maximumSize(1000)
+                .build();
     }
 }
