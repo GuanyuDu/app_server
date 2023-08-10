73e6056b610adcefbee9a6f3079c9a5acd740633
Guanyu <im.dugy@foxmail.com>
2023-08-11 00:00:43 +0800

    移除冗余代码

--- src/main/java/com/guanyu/app/util/TaskRunner.java
+++ src/main/java/com/guanyu/app/util/TaskRunner.java
@@ -1,19 +1,11 @@
 package com.guanyu.app.util;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import lombok.SneakyThrows;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.*;
 
 /**
@@ -23,35 +15,6 @@ import java.util.concurrent.*;
 public class TaskRunner implements ApplicationRunner {
 
     private ArrayList<Future<String>> futures = new ArrayList<>();
-
-    public static void main(String[] args) {
-        String url = "jdbc:mysql://106.52.123.178:3306/mini_app?useUnicode=true&characterEncoding=utf-8&useSSL=true&serverTimezone=UTC";
-        String username = "miniapp";
-        String password = "Pma+911105";
-        String sql = "select * from `comment`";
-        String testSql = "select count(*) from `comment`";
-        try {
-            Connection connection = DriverManager.getConnection(url, username, password);
-            Timer timer = new Timer();
-            timer.schedule(new TimerTask() {
-                @SneakyThrows
-                @Override
-                public void run() {
-                    try (Statement statement = connection.createStatement();
-                         ResultSet resultSet = statement.executeQuery(testSql)) {
-                        while (resultSet.next()) {
-                            System.out.println(LocalTime.now() + " - " + resultSet.getString(1));
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }, 0, 301 * 1000);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     @Override
     public void run(ApplicationArguments args) {
